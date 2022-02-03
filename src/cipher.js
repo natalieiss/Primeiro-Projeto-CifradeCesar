bf6c0bf591ef2b00ddeeb9f13ffdbda3f161add5
Natalie I. S. Santos <natalie_ingrid@hotmail.com>
2022-02-02 21:14:21 -0300

    implemento do textError no cipher.js adicionei a primeira imagem no HTML e aprimorei o texto.

--- src/cipher.js
+++ src/cipher.js
@@ -1,25 +1,32 @@
 const cipher = {
-  
-  encode: (offset, text) => {
-    let textCipher = "";
-    let letters = text.length;
-    for (let i = 0; i < letters; i++) {
-      let asciiNum = text.charCodeAt(i); // pega o codigo corresnpondente a tabela ASC referente as letras digitadas
-      let codeAscii = 65; //corresponde a letra A na tabela ASCII
-      if (asciiNum >= 65 && asciiNum <= 90) { // retorna apenas o alfabeto maiúsculo
-        let encodeValue = ((asciiNum + codeAscii + offset)) % 26 + codeAscii; //calculo para cifrar
-        textCipher = textCipher + String.fromCharCode(encodeValue); //faz o contrario de charcodeat e ao inves de retornar o valor da tabela ascii retorna a letra
-      } else {
-        textCipher = textCipher + String.fromCharCode(asciiNum); //por que esse else?
-      }
 
-    }
+  encode: (offset, text) => {
+      if(Number(text) || text == "" || text == null || offset == "" || offset == null){ 
+        alert("ERRO: use apenas letras no campo de mensagens que a abelhinha ira carregar e não deixe o campo de mensagens em branco");  
+        throw new TypeError("ERRO:", "cipher.js", 4);
+      }
+      let textCipher = "";
+      let letters = text.length;
+      for (let i = 0; i < letters; i++) {
+        let asciiNum = text.charCodeAt(i); // pega o codigo corresnpondente a tabela ASC referente as letras digitadas
+        let codeAscii = 65; //corresponde a letra A na tabela ASCII
+        if (asciiNum >= 65 && asciiNum <= 90) { // retorna apenas o alfabeto maiúsculo
+          let encodeValue = ((asciiNum + codeAscii + offset)) % 26 + codeAscii; //calculo para cifrar
+          textCipher = textCipher + String.fromCharCode(encodeValue); //faz o contrario de charcodeat e ao inves de retornar o valor da tabela ascii retorna a letra
+        } else {
+          textCipher = textCipher + String.fromCharCode(asciiNum); //por que esse else?
+        }
+
+      }
 
     return textCipher;
   },
 
   decode: (offset, text) => {
-
+    if(Number(text) || text == "" || text == null || offset == "" || offset == null){ 
+      alert("ERRO: use apenas letras no campo de mensagens que a abelhinha ira carregar e não deixe o campo de mensagens em branco");  
+      throw new TypeError("ERRO:", "cipher.js", 4);
+    }
     let textDecipher = "";
     let letters = text.length;
     for (let i = 0; i < letters; i++) {
@@ -35,7 +42,7 @@ const cipher = {
     }
 
     return textDecipher;
-  }  
-    }
+  }
+}
 
 export default cipher;
